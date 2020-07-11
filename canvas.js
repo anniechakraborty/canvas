d951f9b5bf99b131c01349bd509be66d1cfce8c4
Annie Chakraborty <the.demented.genius29@gmail.com>
2020-07-11 16:24:17 +0530

    drawn two rudimentary shapes on the screen

--- canvas.js
+++ canvas.js
@@ -1,4 +1,8 @@
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+c.fillRect(200, 100, 80, 80);
+c.fillRect(400, 200, 80, 80);
 console.log(canvas);
